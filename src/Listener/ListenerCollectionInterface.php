f6bbaae9a0bec584c497f872eb42271458e309d0
Alex Patterson <alex.patterson.webdev@gmail.com>
2020-03-25 22:46:02 +0000

    Added new scrutinizer changes after build report failures

--- src/Listener/ListenerCollectionInterface.php
+++ src/Listener/ListenerCollectionInterface.php
@@ -21,16 +21,8 @@ interface ListenerCollectionInterface extends \IteratorAggregate, \Countable
     /**
      * Add a collection of listeners to the collection.
      *
-     * @param callable[] $listeners The collection of event listeners to add.
-     * @param int        $priority  Optional priority for the listener.
+     * @param iterable|callable[] $listeners The collection of event listeners to add.
+     * @param int                 $priority  Optional priority for the listener.
      */
-    public function addListeners(array $listeners, int $priority = 1): void;
-
-    /**
-     * Merge the provided collection into the current one.
-     *
-     * @param \Traversable $collection The collection that should be merged.
-     * @param int          $priority   Optional priority for the listener.
-     */
-    public function merge(\Traversable $collection, int $priority = 1): void;
+    public function addListeners(iterable $listeners, int $priority = 1): void;
 }
